4caf2f59459a3874f38a56d8405be1b4fe0119d8
herlesupreeth <herlesupreeth@gmail.com>
2017-01-18 11:06:26 +0100

    Removed the possibility for double freeing of memory

--- SRC/NAS/EMM/EmmCommon.c
+++ SRC/NAS/EMM/EmmCommon.c
@@ -2,9 +2,9 @@
  * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under 
+ * The OpenAirInterface Software Alliance licenses this file to You under
  * the Apache License, Version 2.0  (the "License"); you may not use this file
- * except in compliance with the License.  
+ * except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
@@ -471,8 +471,12 @@ void emm_common_cleanup (emm_common_data_t *emm_common_data_ctx)
        */
       pthread_mutex_lock(&emm_common_data_head.mutex);
       RB_REMOVE (emm_common_data_map, &emm_common_data_head.emm_common_data_root, emm_common_data_ctx);
-      free_wrapper (emm_common_data_ctx->args);
-      free_wrapper (emm_common_data_ctx);
+      if (emm_common_data_ctx->args) {
+        free_wrapper (emm_common_data_ctx->args);
+      }
+      if (emm_common_data_ctx) {
+        free_wrapper (emm_common_data_ctx);
+      }
       pthread_mutex_unlock(&emm_common_data_head.mutex);
     }
   }
@@ -488,8 +492,12 @@ void emm_common_cleanup_by_ueid (mme_ue_s1ap_id_t ue_id)
     __sync_fetch_and_sub(&emm_common_data_ctx->ref_count, 1);
     pthread_mutex_lock(&emm_common_data_head.mutex);
     RB_REMOVE (emm_common_data_map, &emm_common_data_head.emm_common_data_root, emm_common_data_ctx);
-    free_wrapper (emm_common_data_ctx->args);
-    free_wrapper (emm_common_data_ctx);
+    if (emm_common_data_ctx->args) {
+      free_wrapper (emm_common_data_ctx->args);
+    }
+    if (emm_common_data_ctx) {
+      free_wrapper (emm_common_data_ctx);
+    }
     pthread_mutex_unlock(&emm_common_data_head.mutex);
   }
 }
