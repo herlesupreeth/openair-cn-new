e69a2db43c0da14f4d12d8dd368c1b242adfee32
herlesupreeth <herlesupreeth@gmail.com>
2017-03-28 13:31:54 +0200

    Fix for getting Samsung S7 to work with Openair-CN

--- SRC/NAS/ESM/MSG/PdnConnectivityRequest.c
+++ SRC/NAS/ESM/MSG/PdnConnectivityRequest.c
@@ -2,9 +2,9 @@
  * Licensed to the OpenAirInterface (OAI) Software Alliance under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The OpenAirInterface Software Alliance licenses this file to You under 
+ * The OpenAirInterface Software Alliance licenses this file to You under
  * the Apache License, Version 2.0  (the "License"); you may not use this file
- * except in compliance with the License.  
+ * except in compliance with the License.
  * You may obtain a copy of the License at
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
@@ -48,7 +48,6 @@ decode_pdn_connectivity_request (
    */
   if ((decoded_result = decode_u8_request_type (&pdn_connectivity_request->pdntype, 0, *(buffer + decoded) >> 4, len - decoded)) < 0)
     return decoded_result;
-
   if ((decoded_result = decode_u8_pdn_type (&pdn_connectivity_request->requesttype, 0, *(buffer + decoded) & 0x0f, len - decoded)) < 0)
     return decoded_result;
 
@@ -101,6 +100,8 @@ decode_pdn_connectivity_request (
       break;
 
     default:
+      // Hack. Temporary fix for Samsung Galaxy S7.
+      return decoded;
       errorCodeDecoder = TLV_UNEXPECTED_IEI;
       return TLV_UNEXPECTED_IEI;
     }
